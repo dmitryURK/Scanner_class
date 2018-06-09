30c6e7538e6475d9e34012c2c77fa438ce6da495
дмитрий <dmitryaltair@ukr.net>
2018-06-09 15:49:38 +0300

    1

--- src/com/dmitryUKR/Main.java
+++ src/com/dmitryUKR/Main.java
@@ -1,8 +1,10 @@
 package com.dmitryUKR;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner str = new Scanner(System.in);
+        System.out.println("Input Str: " +str.nextLine());
     }
 }
